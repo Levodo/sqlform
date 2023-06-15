3631b0d82bfaf6310133eda68b60c229f6ea9f54
Levodo Bertrand <levodobertrand@gmail.com>
2023-06-15 21:09:04 +0200

    somme & moyenne

--- requetes.sql
+++ requetes.sql
@@ -45,3 +45,59 @@ select name, population / area_land as  densité
 from facts
 order by densité asc
 limit 1;
+
+
+/*Compter le nombre de lignes en SQL: COUNT*/
+
+select COUNT(*)
+from facts
+;
+
+/*Compter les valeurs non null d'une colonne*/
+
+select COUNT(area_water)
+from facts
+;
+
+select count(birt_rate) from facts;
+
+/*Trouver les valeurs minimales et maximales d'une colonne SQL (MIN & MAX qui ne prend des entiers)*/
+
+select MAX(birth_rate)
+from facts
+;
+
+/*Objectifs:
+
+    1. Utiliser la fonction MIN sur la colonne population_growth.
+    2. Utiliser la fonction MAX sur la colonne death_rate.
+    
+    Travail à réaliser sur DB Browser puis sur Jupyter Notebook
+*/
+
+    select MIN(population_growth)
+    from facts;
+
+    select MAX(death_rate)
+    from facts;
+
+
+/*Calculer des Sommes et des Moyennes en SQL*/
+
+select sum(birth_rate)
+from facts;
+
+select avg(birth_rate)
+from facts
+;
+
+/*Objectifs :
+        1. Utiliser la fonction SUM pour trouver le total de la colonne area_land.
+        2. Utiliser la fonction AVG pour trouver la moyenne de la colonne area_water.
+
+        Travail à réaliser sur DB Browser puis sur jupyter Notebook
+*/
+
+select sum(area_land) from facts;
+
+select avg(area_water) from facts;
